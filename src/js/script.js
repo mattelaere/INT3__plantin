d658b72fad053fe8f8c18b458bc7ead5c6258a45
Isha Mattelaere <isha.mattelaere@student.howest.be>
2025-01-23 15:40:16 +0100

    added shake.js and implemented it

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,35 @@
 import { DotLottie } from '@lottiefiles/dotlottie-web';
 
 
+console.log('Shake event detected'); 
 
+let shakeEvent = new Shake({ threshold: 15, timeout: 1000 });
+shakeEvent.start();
+
+console.log('Shake event detected'); 
+
+window.addEventListener('shake', shakeEventDidOccur, false);
+
+function shakeEventDidOccur() {
+    console.log('Shake event detected'); 
+    const normalBooks = document.querySelectorAll('.normal__book');
+    const anonymousMore = document.querySelector('.anonymous__more');
+    const phoneShaker = document.querySelector('.phone__shaker');
+
+    let opacity = 1;
+    const interval = setInterval(() => {
+        opacity -= 0.25;
+        normalBooks.forEach(book => {
+            book.style.opacity = opacity;
+        });
+        phoneShaker.style.opacity = opacity;
+
+        if (opacity <= 0) {
+            clearInterval(interval);
+            anonymousMore.classList.add('visible');
+        }
+    }, 500);
+}
 
 new DotLottie({
     autoplay: false,
