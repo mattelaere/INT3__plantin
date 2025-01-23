7120e9edf5718604a52b9af267f45fdc3cb15973
Isha Mattelaere <isha.mattelaere@student.howest.be>
2025-01-23 16:30:05 +0100

    keep the shake down for the moment, css changes

--- src/js/script.js
+++ src/js/script.js
@@ -2,43 +2,13 @@
 import { DotLottie } from '@lottiefiles/dotlottie-web';
 
 
-console.log('Shake event detected'); 
-
-let shakeEvent = new Shake({ threshold: 15, timeout: 1000 });
-shakeEvent.start();
-
-console.log('Shake event detected'); 
-
-window.addEventListener('shake', shakeEventDidOccur, false);
-
-function shakeEventDidOccur() {
-    console.log('Shake event detected'); 
-    const normalBooks = document.querySelectorAll('.normal__book');
-    const anonymousMore = document.querySelector('.anonymous__more');
-    const phoneShaker = document.querySelector('.phone__shaker');
-
-    let opacity = 1;
-    const interval = setInterval(() => {
-        opacity -= 0.25;
-        normalBooks.forEach(book => {
-            book.style.opacity = opacity;
-        });
-        phoneShaker.style.opacity = opacity;
-
-        if (opacity <= 0) {
-            clearInterval(interval);
-            anonymousMore.classList.add('visible');
-        }
-    }, 500);
-}
-
 new DotLottie({
     autoplay: false,
     loop: false,
     canvas: document.querySelector('.the__coin canvas'),
     src: `${import.meta.env.BASE_URL}animations/known__coin.json`,
-
 });
+
 
 new DotLottie({
     autoplay: true,
@@ -47,6 +17,43 @@ new DotLottie({
     src: `${import.meta.env.BASE_URL}animations/balance.json`,
 });
 
+
+
+
+
+
+
+
+// console.log('Shake event detected'); 
+
+// var shakeEvent = new Shake({ threshold: 15, timeout: 1000 });
+// shakeEvent.start();
+
+// console.log('Shake event detected'); 
+
+// window.addEventListener('shake', shakeEventDidOccur, false);
+
+// function shakeEventDidOccur() {
+//     console.log('Shake event detected'); 
+//     const normalBooks = document.querySelectorAll('.normal__book');
+//     const anonymousMore = document.querySelector('.anonymous__more');
+//     const phoneShaker = document.querySelector('.phone__shaker');
+
+//     let opacity = 1;
+//     const interval = setInterval(() => {
+//         opacity -= 0.25;
+//         normalBooks.forEach(book => {
+//             book.style.opacity = opacity;
+//         });
+//         phoneShaker.style.opacity = opacity;
+
+//         if (opacity <= 0) {
+//             clearInterval(interval);
+//             anonymousMore.classList.add('visible');
+//         }
+//     }, 500);
+//     alert('shake!');
+// }
 
 
 
