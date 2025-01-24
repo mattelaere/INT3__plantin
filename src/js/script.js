93f65374a84da5c8b040870b4e11f79965acf85b
Isha Mattelaere <isha.mattelaere@student.howest.be>
2025-01-24 18:33:43 +0100

    multiple interactions and tryout shake (again)

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,22 @@
 
 import { DotLottie } from '@lottiefiles/dotlottie-web';
-const draggableItems = document.querySelectorAll('.robbery__images img[draggable="true"]');
-const dropBox = document.getElementById('drop-box');
-const robberyText = document.getElementById('robbery-text');
-const robberyArrow = document.querySelector('.robbery__arrow');
-const robberyTextImage = document.querySelector('.robbery__text');
-let droppedItemsCount = 0;
+gsap.registerPlugin(ScrollTrigger);
+
+
+
+
+gsap.from(".slide__over", {
+    y: 300,
+    scrollTrigger: {
+        trigger: ".detective__logo__bg",
+        start: "top bottom",
+        end: "top 10%",
+
+        scrub: true,
+    }
+
+});
+
 
 new DotLottie({
     autoplay: false,
@@ -22,6 +33,19 @@ new DotLottie({
     src: `${import.meta.env.BASE_URL}animations/balance.json`,
 });
 
+
+
+
+
+
+const draggableItems = document.querySelectorAll('.robbery__images img[draggable="true"]');
+const dropBox = document.getElementById('drop-box');
+const robberyText = document.getElementById('robbery-text');
+const robberyArrow = document.querySelector('.robbery__arrow');
+const robberyTextImage = document.querySelector('.robbery__text');
+let droppedItemsCount = 0;
+
+
 document.querySelector('.guilty__btn').addEventListener('click', () => {
     const verdictSection = document.querySelector('.verdict__section');
     verdictSection.classList.remove('innocent');
@@ -37,24 +61,19 @@ document.querySelector('.innocent__btn').addEventListener('click', () => {
 
 
 draggableItems.forEach(item => {
-    item.addEventListener('dragstart', dragStartHandler);
+    item.addEventListener('dragstart', (e) => {
+        console.log('Drag start:', e.target.id);
+        e.dataTransfer.setData('text/plain', e.target.id);
+    });
 });
 
-dropBox.addEventListener('dragover', dragOverHandler);
-dropBox.addEventListener('drop', dropHandler);
-
-function dragStartHandler(e) {
-    console.log('Drag start:', e.target.id);
-    e.dataTransfer.setData('text/plain', e.target.id);
-}
-
-function dragOverHandler(e) {
+dropBox.addEventListener('dragover', (e) => {
     e.preventDefault();
     dropBox.classList.add('drag-over');
+    console.log('Drag over');
+});
 
-}
-
-function dropHandler(e) {
+dropBox.addEventListener('drop', (e) => {
     e.preventDefault();
     dropBox.classList.remove('drag-over');
     const id = e.dataTransfer.getData('text/plain');
@@ -76,4 +95,51 @@ function dropHandler(e) {
             }, 300);
         }
     }
-}
+});
+
+const requestT = () => {
+    document.querySelector(".shake__button").classList.add('hidden');
+
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        DeviceMotionEvent.requestPermission()
+            .then(response => {
+                if (response == 'granted') {
+                    window.addEventListener('devicemotion', handleDeviceMotion, false);
+                }
+            })
+            .catch(console.error);
+    } else {
+        window.addEventListener('devicemotion', handleDeviceMotion);
+    }
+};
+
+
+const handleDeviceMotion = (e) => {
+    if (window.innerWidth < 48 * 16) { // 48em in pixels
+        const anonymousSection = document.querySelector('.anonymous__interaction__section');
+        const rect = anonymousSection.getBoundingClientRect();
+        const isVisible = rect.top >= 0 && rect.bottom <= window.innerHeight;
+
+        if (isVisible && (e.rotationRate.alpha > 256 || e.rotationRate.beta > 256 || e.rotationRate.gamma > 256)) {
+            document.querySelectorAll('.normal__book').forEach(img => {
+                img.classList.add('hide');
+            });
+            document.querySelector('.anonymous__more').style.display = 'block';
+        }
+    }
+};
+
+
+const init = () => {
+    document.querySelector(".shake__button").addEventListener("click", requestT);
+};
+
+
+init();
+
+
+
+
+
+
+
